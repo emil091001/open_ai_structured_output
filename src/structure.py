360f96f26a5bf5ca5703923d9780d7bbb0b56348
emil091001 <emil091001@gmail.com>
2025-03-27 10:47:36 +0100

    new structure

--- src/structure.py
+++ src/structure.py
@@ -3,7 +3,7 @@ from typing import List, Literal, TypeVar, Union
 from pydantic import BaseModel
 
 
-Module = Literal["A1-A3", "A1", "A2", "A3", "A4", "A5", "B1",
+Module = Literal["A1-A3", "A1A3", "A1", "A2", "A3", "A4", "A5", "B1",
                  "B2", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3", "C4", "D"]
 
 EnvironmentalImpactParameterName = Literal[
@@ -67,19 +67,21 @@ EndOfLifeFlowParameterName = Literal[
 class Value(BaseModel):
     value: Union[str, None]
     module: Module
+    scenario: Union[str, None]
 
 
 class Parameter[T](BaseModel):
     parameter: T
-    unit: str
     values: List[Value]
 
+class DeclaredUnit(BaseModel):
+    unit: Union[str, None]
+    amount: int
 
 class Structure(BaseModel):
     product_name: str
     epd_id: str
-    # valid_to: str
-    # ref_year: str
+    declared_unit: DeclaredUnit
     compliance: List[str]
     producer_name: Union[str, None]
     environmental_impact: List[Parameter[EnvironmentalImpactParameterName]]
